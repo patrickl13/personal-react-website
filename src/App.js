ad67e74326f2eb7a137879a2a9dd0b3c20bba80d
Patrick <patrick.leduc13@gmail.com>
2019-08-24 23:15:31 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { NavigationBar } from './components/navbar/navbar.component';
+import { Info } from './components/aboutme/aboutme.component';
+import { Banner } from './components/banner/banner.component';
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavigationBar />
+        <Banner />
+        <Info />
+      </React.Fragment>
+      
+      
+    );
+  }
 }
 
 export default App;
---
+++ src/components/aboutme/aboutme.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Image, Table } from 'react-bootstrap';
+
+
+export const Info = () => (
+
+    <Table>
+        <tr>
+            <td></td>
+            <td><Image src="logo192.png" rounded /></td>
+            <td> Here is some info for ya</td>
+            <td></td>
+        </tr>
+    </Table>
+
+);
